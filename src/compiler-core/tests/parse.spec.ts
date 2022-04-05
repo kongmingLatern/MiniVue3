b444244cccf60b286c0ff5ce2ed88a2c83b3d19c
凤之兮原 <9673538+mo-wind@user.noreply.gitee.com>
2022-04-05 22:09:01 +0800

    实现解析插值 + element标签 + text 功能 + 联合类型 template

--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -1,5 +1,5 @@
 import { NodeTypes } from "../src/ast";
-import { baseParse } from "../src/parse";
+import { baseParse } from '../src/parse';
 
 describe('Parse', () => {
   describe('interpolation', () => {
@@ -15,4 +15,80 @@ describe('Parse', () => {
       })
     });
   });
-});
+
+  describe('element', () => {
+    it('simple element div', () => {
+      const ast = baseParse("<div></div>")
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: "div",
+        children: []
+      })
+    });
+  });
+
+  describe('text', () => {
+    it('simple text', () => {
+      const ast = baseParse("Common Text")
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.TEXT,
+        content: "Common Text"
+      })
+    });
+  });
+
+  test('hello world', () => {
+    const ast = baseParse("<p>hi,{{message}}</p>")
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: "p",
+      children: [
+        {
+          type: NodeTypes.TEXT,
+          content: "hi,"
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: "message"
+          }
+        }
+      ]
+    })
+  });
+
+  test('Nested element', () => {
+    const ast = baseParse("<div><p>hi</p>{{message}}</div>")
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: "div",
+      children: [
+        {
+          type: NodeTypes.ELEMENT,
+          tag: "p",
+          children: [
+            {
+              type: NodeTypes.TEXT,
+              content: "hi"
+            }
+          ]
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: "message"
+          }
+        }
+      ]
+    })
+  });
+
+  test('should throw error when lack end tag', () => {
+    // baseParse("<div><span></div>")
+    expect(() => {
+      baseParse("<div><span></div>")
+    }).toThrow("缺少结束标签：span")
+  });
+})
