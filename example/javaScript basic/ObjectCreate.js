fb1ac03c07ec73312254c7c51c458b53972ee604
施颖杰 <1577310596@qq.com>
2022-04-20 21:41:59 +0800

    update: create Demo for object.create

---
+++ example/javaScript basic/ObjectCreate.js
@@ -0,0 +1,12 @@
+const a = {  rep : 'apple' }
+const b = new Object(a)
+console.log(b) // {rep: "apple"}
+console.log(b.__proto__) // {}
+console.log(b.rep) // {rep: "apple"}
+
+// Object.create() 方式创建
+const a = { rep: 'apple' }
+const b = Object.create(a)
+console.log(b)  // {}
+console.log(b.__proto__) // {rep: "apple"}
+console.log(b.rep) // {rep: "apple"}
